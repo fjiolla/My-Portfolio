41408e4dca62ccbfb10fec2184f52d6cf2f66fb5
Leena Shah <leenashah065@gmail.com>
2025-10-24 00:19:28 +0530

    Fixed config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/My-Portfolio/",
+  base: "/",
 });
